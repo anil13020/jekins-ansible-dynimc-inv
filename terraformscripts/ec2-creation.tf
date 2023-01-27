a6907460f59815e2212211272bb750daf9f21f6c
Anil <68263669+anil13020@users.noreply.github.com>
2023-01-27 14:54:33 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,15 +1,15 @@
 provider "aws"{
-region = "ap-southeast-2"
+region = "us-east-1"
 }
 
 
 resource "aws_instance" "AWSInstance"{
      ami = "ami-08cd0547bf85331e7"
 	 instance_type = "t2.micro"
-	 security_groups = ["launch-wizard-2"]
-	 key_name = "devopstrainingmss"
+	 security_groups = ["launch-wizard-1"]
+	 key_name = "ansible_key"
 	 tags = {
-	 Name = "tomcatservers"
+	 Name = "tomcat"
 	 }
 
 }
